b1971f8dd2b8973716d119b2622deb08f020deb8
PulsarG <furylonzero@gmail.com>
2022-09-03 17:39:14 +0500

    ver017.3

--- src/shopcomponents/WeaponsShop.vue
+++ src/shopcomponents/WeaponsShop.vue
@@ -1,47 +1,82 @@
 <template>
-    <div class="list">
-
-        <submenuwepons-shop :Items="PistolsShop" :NameShop="Pistols"></submenuwepons-shop>
-
-        <!--  <body-shop></body-shop> -->
-        <custom-weapons></custom-weapons>
-
-    </div>
+  <div class="list">
+    <submenuwepons-shop
+      :Items="PistolsShop"
+      :NameShop="Pistols"
+    ></submenuwepons-shop>
+    <submenuwepons-shop :Items="SmgShop" :NameShop="Smg"></submenuwepons-shop>
+    <submenuwepons-shop :Items="RifShop" :NameShop="Rif"></submenuwepons-shop>
+    <submenuwepons-shop :Items="ShtShop" :NameShop="Sht"></submenuwepons-shop>
+    <submenuwepons-shop :Items="HvyShop" :NameShop="Hvy"></submenuwepons-shop>
+    <submenuwepons-shop
+      :Items="MeeleShop"
+      :NameShop="Meele"
+    ></submenuwepons-shop>
+    <submenuwepons-shop
+      :Items="ExoticShop"
+      :NameShop="Exotic"
+    ></submenuwepons-shop>
+    <weponsoption-shop
+      :Items="AmmoShop"
+      :NameShop="Ammo"
+    ></weponsoption-shop>
+    <!--  <body-shop></body-shop> -->
+    <custom-weapons></custom-weapons>
+  </div>
 </template>
 
 <script>
 import SubmenuweponsShop from "./SubmenuweponsShop.vue";
+import WeponsoptionShop from "./WeponsoptionShop.vue";
 import CustomWeapons from "./CustomWeapons.vue";
 
 export default {
-    components: { SubmenuweponsShop, CustomWeapons },
-    data() {
-        return {
-            Pistols: "Пистолеты",
-        }
+  components: { SubmenuweponsShop, CustomWeapons, WeponsoptionShop },
+  data() {
+    return {
+      Pistols: "Пистолеты",
+      Smg: "Пистолеты-Пулеметы",
+      Rif: "Штурмовые",
+      Sht: "Дробовики",
+      Hvy: "Тяжелое",
+      Meele: "Ближнее",
+      Exotic: "Экзотик",
+      Ammo: "Боеприпасы и опции",
+    };
+  },
+  computed: {
+    PistolsShop() {
+      return this.$store.state.weaponsshop.Pistols;
     },
-    computed: {
-        PistolsShop() {
-            return this.$store.state.weaponsshop.Pistols;
-        },
-
-        BodyShop() {
-            return this.$store.state.cyberneticsshop.Body;
-        },
-        BioUpgrade() {
-            return this.$store.state.cyberneticsshop.Bioupgrade;
-        },
-        MeleeWeapons() {
-            return this.$store.state.cyberneticsshop.Meleeweapons;
-        }
+    SmgShop() {
+      return this.$store.state.weaponsshop.Smg;
     },
-}
+    RifShop() {
+      return this.$store.state.weaponsshop.Rif;
+    },
+    ShtShop() {
+      return this.$store.state.weaponsshop.Sht;
+    },
+    HvyShop() {
+      return this.$store.state.weaponsshop.Hvy;
+    },
+    MeeleShop() {
+      return this.$store.state.weaponsshop.Meele;
+    },
+    ExoticShop() {
+      return this.$store.state.weaponsshop.Exotic;
+    },
+    AmmoShop() {
+      return this.$store.state.weaponsshop.Ammo;
+    },
+  },
+};
 </script>
 
 <style scoped>
 .list {
-    display: flex;
-    flex-direction: column;
-    margin-left: 15%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 15%;
 }
 </style>
--- src/shopcomponents/WeponsoptionShop.vue
+++ src/shopcomponents/WeponsoptionShop.vue
@@ -7,32 +7,17 @@
   <table class="table" v-show="isShow">
     <thead>
       <tr class="item">
-        <th class="type" style="color: blue">Type</th>
         <th class="nameitem" style="color: blue">NAME</th>
-        <th class="wa" style="color: blue">WA</th>
-        <th class="con" style="color: blue">Con</th>
-        <th class="avail" style="color: blue">Avail</th>
-        <th class="damage" style="color: blue">Damage</th>
-        <th class="shots" style="color: blue">Shots</th>
-        <th class="rof" style="color: blue">Rof</th>
-        <th class="rel" style="color: blue">Rel</th>
-        <th class="range" style="color: blue">Range</th>
+        <th class="range" style="color: blue">Amount</th>
         <th class="price" style="color: blue">Price</th>
         <th class="btn" style="color: blue"></th>
       </tr>
     </thead>
     <tbody>
       <tr class="item" v-for="i in Items" :key="i.name">
-        <td class="type">{{ i.type }}</td>
+        
         <th class="nameitem">{{ i.name }}</th>
-        <td class="wa">{{ i.wa }}</td>
-        <td class="con">{{ i.con }}</td>
-        <th class="avail">{{ i.avail }}</th>
-        <td class="damage">{{ i.damage }}</td>
-        <td class="shots">{{ i.shots }}</td>
-        <td class="rof">{{ i.rof }}</td>
-        <th class="rel">{{ i.rel }}</th>
-        <td class="range">{{ i.range }}</td>
+        <td class="range">{{ i.amount }}</td>
         <td class="price">{{ i.price }}</td>
         <button class="btn" @click="addWeapons(i)">+ BUY</button>
       </tr>
@@ -96,78 +81,23 @@ export default {
 }
 
 .price {
-  width: 10%;
+  width: 15%;
   border: 1px solid black;
   display: flex;
   justify-content: center;
 }
 
 .nameitem {
-  width: 30%;
+  width: 50%;
   border: 1px solid black;
   display: flex;
   justify-content: flex-start;
   padding-left: 10px;
 }
 
-.type {
-  width: 20%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.wa {
-  width: 10%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.con {
-  width: 10%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.rof {
-  width: 10%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.rel {
-  width: 10%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.avail {
-  width: 10%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.damage {
-  width: 18%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
-
-.shots {
-  width: 10%;
-  border: 1px solid black;
-  display: flex;
-  justify-content: center;
-}
 
 .range {
-  width: 10%;
+  width: 15%;
   border: 1px solid black;
   display: flex;
   justify-content: center;
